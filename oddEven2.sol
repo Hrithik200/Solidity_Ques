a4f0958b7d6c1542cbfaacfa426aefb960195b27
Hrithik200 <64530006+Hrithik200@users.noreply.github.com>
2023-05-25 23:10:45 +0530

    Added Seoncd Contract
    Odd Even Contract

---
+++ oddEven2.sol
@@ -0,0 +1,13 @@
+// write a solidity program to check whether a given number is even or odd
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract EvenOddChecker {
+    function isEven(uint256 number) public pure returns (bool) {
+        if (number % 2 == 0) {
+            return true; // Even number
+        } else {
+            return false; // Odd number
+        }
+    }
+}
