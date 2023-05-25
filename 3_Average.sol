d094d3c321e99f196b60b3a6e7bc07f693e746e4
Hrithik200 <64530006+Hrithik200@users.noreply.github.com>
2023-05-25 23:22:36 +0530

    update
    Update to 3

---
+++ 3_Average.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract AverageCalculator {
+    function calculateAverage(uint256 num1, uint256 num2, uint256 num3) public pure returns (uint256) {
+        uint256 sum = num1 + num2 + num3;
+        uint256 average = sum / 3;
+        return average;
+    }
+}
